ea4359854c8b46ca579d9d6bfe46e058f34668d6
Nino Nussbaumer <6767-nino.nussbaumer@users.noreply.code.swisscom.com>
2024-09-03 10:19:23 +0200

    removed Modal

--- app/(tabs)/detailedCollection.tsx
+++ app/(tabs)/detailedCollection.tsx
@@ -1,65 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Modal, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { AddPlantForm } from '@/components/addPlantForm';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ActionButton } from '@/components/actionButtton';
 
-export default function DetailedCollectionScreen() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+export default function DetailedCollection() {
   const [inputValue, setInputValue] = useState('');
-  const [isInputValid, setIsInputValid] = useState(false);
-
-  useEffect(() => {
-    const firstTime = true;
-    if (firstTime) {
-      setIsModalVisible(true);
-    }
-  }, []);
-
-  const handleInputChange = (text: string) => {
-    setInputValue(text);
-    const isValid = /[a-zA-Z0-9]/.test(text);
-    setIsInputValid(isValid);
-  };
-  
-
-  const handleModalClose = () => {
-    if (isInputValid) {
-      setIsModalVisible(false);
-    } else {
-      alert("Please enter a valid name containing at least one letter or number.");
-    }
-  };
 
   return (
     <ParallaxScrollView headerText={inputValue}>
-      <Text>Detailed Collection Page</Text>
-      <AddPlantForm />
-
-      {/* Popup Modal */}
-      <Modal
-        visible={isModalVisible}
-        transparent={true}
-        animationType="slide"
-        onRequestClose={handleModalClose}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalTitle}>Enter a name for your first Collection!</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="f.e. Home"
-              value={inputValue}
-              onChangeText={handleInputChange}
-            />
-            <ActionButton
-              title="Submit"
-              onPress={handleModalClose}
-              disabled={!isInputValid}
-            />
-          </View>
-        </View>
-      </Modal>
+      <View style={styles.titleContainer}>
+        <Text>Detailed Collection Page</Text>
+        <AddPlantForm />
+      </View>
     </ParallaxScrollView>
   );
 }
@@ -68,30 +20,5 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    width: 300,
-    padding: 20,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  modalTitle: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
   },
 });
