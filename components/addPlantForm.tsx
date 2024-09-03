9f197190f6e72c4bd619040f828998e565dcc929
mb8144 <maurusbrunnschweiler@gmail.com>
2024-09-03 11:17:14 +0200

    WIP

--- components/addPlantForm.tsx
+++ components/addPlantForm.tsx
@@ -1,37 +1,67 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, Text, StyleSheet } from "react-native";
-import CameraScreen from "./CameraScreen";
+import React, { useRef, useState } from "react";
+import { View, TextInput, Button, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { CameraType, CameraView } from "expo-camera";
+import { Plant } from "@/data/models";
+import { insertData } from "@/services/DatabaseService";
 
 export function AddPlantForm() {
-  const [plantName, setPlantName] = useState("");
-  const [timesPerWeek, setTimesPerWeek] = useState("");
-  const [plantImage, setPlantImage] = useState("");
-  const [amount, setAmount] = useState("");
-  const [cameraScreenVisible, setCameraScreenVisible] = useState(false); // Correct state management
+  const [plantName, setPlantName] = useState<string>("");
+  const [timesPerWeek, setTimesPerWeek] = useState<number>(0);
+  const [amount, setAmount] = useState<number>(0);
+  const [plantImage, setPlantImage] = useState<Blob | null>(null);
+  const [cameraScreenVisible, setCameraScreenVisible] = useState<boolean>(false);
+  const [facing, setFacing] = useState<CameraType>('back');
+
+  const cameraRef = useRef<CameraView>(null);
 
   const handlePlantNameChange = (text: string) => {
     setPlantName(text);
   };
 
   const handleTimesPerWeekChange = (text: string) => {
-    setTimesPerWeek(text);
-  };
-
-  const handlePlantImageChange = (text: string) => {
-    setPlantImage(text);
+    setTimesPerWeek(Number(text));
   };
 
   const handleAmountChange = (text: string) => {
-    setAmount(text);
-  };
-
-  const handleSubmit = () => {
-    console.log("submitted");
+    setAmount(Number(text));
   };
 
   const handleCameraIconClick = () => {
     setCameraScreenVisible((prevState) => !prevState);
+  };
+
+  const toggleCameraFacing = () => {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  };
+
+  const takePhoto = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync({ base64: true });
+      const response = await fetch(photo.uri);
+      const blob = await response.blob();
+      setPlantImage(blob);
+      setCameraScreenVisible(false);
+      alert('successfully')
+    }
+  };
+
+  
+
+  const handleSubmit = async () => {
+    console.log('Add plant button pressed');
+    let tableName = "Plant";
+    let data: Plant = {
+      title: plantName,
+      frequency: timesPerWeek,
+      waterAmount: amount,
+      collectionId: 2, 
+      image: plantImage || new Blob(), 
+    };
+
+    await insertData(tableName, data);
+    console.log('inserted: ', data);
+  
   };
 
   return (
@@ -48,8 +78,9 @@ export function AddPlantForm() {
         <Text style={styles.label}>Times Per Week:</Text>
         <TextInput
           style={styles.input}
-          value={timesPerWeek}
+          value={timesPerWeek.toString()}
           onChangeText={handleTimesPerWeekChange}
+          keyboardType="numeric"
         />
       </View>
 
@@ -57,8 +88,9 @@ export function AddPlantForm() {
         <Text style={styles.label}>Amount:</Text>
         <TextInput
           style={styles.input}
-          value={amount}
+          value={amount.toString()}
           onChangeText={handleAmountChange}
+          keyboardType="numeric"
         />
       </View>
 
@@ -73,11 +105,22 @@ export function AddPlantForm() {
       {cameraScreenVisible && (
         <View>
           <Ionicons name="close" size={30} onPress={handleCameraIconClick} />
-          <CameraScreen />
+          <View style={styles.container}>
+            <CameraView ref={cameraRef} style={styles.camera} facing={facing}>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                  <Text style={styles.text}>Flip Camera</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={takePhoto}>
+                  <Text style={styles.text}>Take Photo</Text>
+                </TouchableOpacity>
+              </View>
+            </CameraView>
+          </View>
         </View>
       )}
-      <Button title="Save" onPress={handleSubmit} />
-      <Button title="Back" onPress={handleSubmit} />
+      <Button title="Save" onPress={handleSubmit}/>
+      <Button title="Back" onPress={() => console.log("Back button pressed")} />
     </View>
   );
 }
@@ -102,5 +145,28 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
